ec5a42b403bccc170c34432cdccd5128dd538c1a
Jayshri Landge <113108307+jayshrilandge30@users.noreply.github.com>
2024-10-07 19:25:54 +0530

    Add files via upload

--- login.php
+++ login.php
@@ -1,124 +1,158 @@
+<?php
+
+session_start();
+
+if (isset($_SESSION['id_user']) || isset($_SESSION['id_company'])) {
+  header("Location: index.php");
+  exit();
+}
+
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Andada+Pro:ital@1&display=swap" rel="stylesheet">
-    <!-- Favicons -->
-    <link href="img/logo.png" rel="icon">
-    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-    <!-- Google Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Placement Portal</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
 
-    <!-- Vendor CSS Files -->
-    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <!-- <link rel="stylesheet" href="css/AdminLTE.min.css"> -->
+  <!-- <link rel="stylesheet" href="css/_all-skins.min.css"> -->
+  <!-- Custom -->
+  <link rel="stylesheet" href="css/custom.css">
+  <script src="https://cdn.tailwindcss.com"></script>
 
-    <!-- Variables CSS Files. Uncomment your preferred color scheme -->
-    <link href="assets/css/variables.css" rel="stylesheet">
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
 
-
-    <!-- Template Main CSS File -->
-    <link href="assets/css/main.css" rel="stylesheet">
-
-    <!-- custom css -->
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-    <link rel="stylesheet" href="css/AdminLTE.min.css">
-    <link rel="stylesheet" href="css/_all-skins.min.css">
-    <link rel="stylesheet" href="assets/css/chat.css">
-    <title>Login</title>
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
 
-<body>
+<body class="hold-transition skin-green sidebar-mini">
+  <div class="wrapper">
+
+    <header class="main-header flex mx-8 mt-4">
+
+      <!-- Logo -->
+      <!-- <a href="index.php" class="logo logo-bg">
+        <!-- mini logo for sidebar mini 50x50 pixels -->
+      <!-- <span class="logo-mini"><b>J</b>P</span> -->
+      <!-- logo for regular state and mobile devices -->
+      <!-- <span class="logo-lg text-white bg-black"><b>Placement</b> Portal</span> -->
+      </a>
+
+      <a class="text-white bg-black"> Placement Portal</a>
+
+      <!-- Header Navbar: style can be found in header.less -->
+      <nav class="navbar navbar-static-top">
+        <!-- Navbar Right Menu -->
+        <div class="navbar-custom-menu">
+          <ul class="nav navbar-nav mt-8">
+            <li>
+              <a href="jobs.php">Jobs</a>
+            </li>
+            <?php if (empty($_SESSION['id_user']) && empty($_SESSION['id_company'])) { ?>
+              <li>
+                <a href="login.php">Login</a>
+              </li>
+              <li>
+                <a href="sign-up.php">Sign Up</a>
+              </li>
+              <?php } else {
+
+              if (isset($_SESSION['id_user'])) {
+              ?>
+                <li>
+                  <a href="user/index.php">Dashboard</a>
+                </li>
+              <?php
+              } else if (isset($_SESSION['id_company'])) {
+              ?>
+                <li>
+                  <a href="company/index.php">Dashboard</a>
+                </li>
+              <?php } ?>
+              <li>
+                <a href="logout.php">Logout</a>
+              </li>
+            <?php } ?>
+          </ul>
+        </div>
+      </nav>
+    </header>
 
 
-    <?php
 
-    include 'php/header.php'
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper" style="margin-left: 0px;">
 
-    ?>
-    <section class="text-gray-600 body-font">
-        <div class="container px-5 py-24">
-            <div class="flex flex-wrap -m-4">
-                <div class="p-4 md:w-1/3">
-                    <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                        <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="assets/img/login/login-2.jpg" alt="blog">
-                        <div class="p-6">
-                            <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">Are you a </h2>
-                            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Student?</h1>
-                            <p class="leading-relaxed mb-3">Students can login using their credentials. New students can register.</p>
-                            <div class="flex items-center flex-wrap ">
-                                <a href="login-candidates.php" class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Login/Register
-                                    <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                        <path d="M5 12h14"></path>
-                                        <path d="M12 5l7 7-7 7"></path>
-                                    </svg>
-                                </a>
-
-                            </div>
-                        </div>
-                    </div>
+      <section class="content-header">
+        <div class="container">
+          <div class="row latest-job margin-top-50 margin-bottom-20">
+            <h3 class="text-center margin-bottom-20">Log into your account</h3>
+            <div class="col-md-6 latest-job ">
+              <div class="small-box bg-yellow padding-5">
+                <div class="inner">
+                  <h3 class="text-center">Are you a student ?</h3>
                 </div>
-                <div class="p-4 md:w-1/3 ">
-                    <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                        <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="assets/img/login/login-1.jpg" alt="blog">
-                        <div class="p-6">
-                            <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">Are you from the</h2>
-                            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Placement Cell ?</h1>
-                            <p class="leading-relaxed mb-3">The placement coordinators can log into their account from here.</p>
-                            <div class="flex items-center flex-wrap">
-                                <a href="login-company.php" class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Login/Register
-                                    <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                        <path d="M5 12h14"></path>
-                                        <path d="M12 5l7 7-7 7"></path>
-
-                                    </svg>
-                                </a>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="p-4 md:w-1/3 bg-gray-700" style="font-family: 'Andada Pro', serif; color:#d1b8c6">
-                    <ol list-style:upper-roman>
-                        <li>Placement is a decisive factor of successful completion of any coursework at the graduate or postgraduate level.</li>
-                        <br>
-                        <li>It is a dream of every student to get placed in top MNCs to achieve their set goals and objectives.</li>
-                        <br>
-                        <li> The private universities are also levelling up their game by equipping and upgrading their training and placement cells.</li>
-                        <br>
-                        <li> Contrary to popular belief, the role of the placement cell is not limited to organisingrecruitment drive at the campus.</li>
-                        <br>
-                        <li> They play a much broader role in placing a student in a reputed organisation. Let’s find out how!</li>
-                        <br>
-                        <li style="text-align:right">- Training and Placement Officer</li>
-
-                    </ol>
-                </div>
+                <a href="login-candidates.php" class="small-box-footer">
+                  Login <i class="fa fa-arrow-circle-right"></i>
+                </a>
+              </div>
             </div>
-
+            <div class="col-md-6 latest-job ">
+              <div class="small-box bg-red padding-5">
+                <div class="inner">
+                  <h3 class="text-center">Placement Cell</h3>
+                </div>
+                <a href="login-company.php" class="small-box-footer">
+                  Login <i class="fa fa-arrow-circle-right"></i>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-        <div>
+      </section>
 
 
-        </div>
-    </section>
 
+    </div>
+    <!-- /.content-wrapper -->
 
-    <?php
-    include 'php/footer.php'
-    ?>
+    <footer class="main-footer" style="margin-left: 0px;">
+      <div class="text-center">
+        <strong>Copyright &copy; 2022 <a href="learningfromscratch.online">Placement Portal</a>.</strong> All rights
+        reserved.
+      </div>
+    </footer>
+
+    <!-- /.control-sidebar -->
+    <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+    <div class="control-sidebar-bg"></div>
+
+  </div>
+  <!-- ./wrapper -->
+
+  <!-- jQuery 3 -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <!-- Bootstrap 3.3.7 -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="js/adminlte.min.js"></script>
 </body>
 
 </html>
